9a6d368459f5d55c732eb9361c6aa0c542be6856
Jnstiu <44180994+Jnstiu@users.noreply.github.com>
2018-11-16 22:36:35 -0500

    Update and rename Project2.txt to Project2.s
    Changed to 600 bits for 150-word input.  Included main function.

--- Project2.s
+++ Project2.s
@@ -1,8 +1,8 @@
 .data
-        str0: .asciiz "Input is too long." #Displays and string gets added to data - long input string
-	str1: .asciiz "Input is empty." #String is added to data - empty input
-        str2: .asciiz "Invalid base-34 number." #String is added to data - invalid input
-	str3: .space 8 #Sets aside space for input of 8 bits
+        str0: .asciiz "Input is too long." #Displays if error occurs - long input string
+	str1: .asciiz "Input is empty." #Displays if error occurs - empty input
+        str2: .asciiz "Invalid base-34 number." #Displays if error occurs - invalid input
+	str3: .space 600 #Sets aside space for input of 600 bits or 150 words
 
 .text
 str0Error: #For long input
@@ -22,4 +22,8 @@ str3Error: #For invalid input
   li $v0, 4 #Address of string that gets printed
   syscall
   j exit #Jumps to exit of program
-  
+main:
+  li $v0, 8 
+  la $a0, str3 
+  li $a1, 300
+  syscall
